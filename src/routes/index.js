9dcefd4d2b8b8631b25713dafec92cee1ac501c6
MacOS <macos@Mac-cua-Giang.local>
2022-06-13 12:17:04 +0700

    set up default layout

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,19 @@
 // Layouts
 import { HeaderOnly } from '~/components/Layout';
 
-import Search from '~/pages/Search';
+import Home from '~/pages/Home';
 import Following from '~/pages/Following';
 import Profile from '~/pages/Profile';
 import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
 
 // Public Routes
 export const publicRoutes = [
-  { path: '/', component: Search },
+  { path: '/', component: Home },
   { path: '/following', component: Following },
   { path: '/profile', component: Profile },
   { path: '/upload', component: Upload, layout: HeaderOnly },
+  { path: '/upload', component: Search, layout: null },
 ];
+
 export const privatecRoutes = [];
