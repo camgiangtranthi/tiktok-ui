10b5a91a0838b6d009d7dc325edcce292dfe3f5e
MacOS <macos@Mac-cua-Giang.local>
2022-06-17 16:38:35 +0700

    set up UI header #3

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -4,6 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
 
+import Button from '~/components/Button';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
@@ -50,7 +51,13 @@ export default function Header() {
                         </button>
                     </div>
                 </Tippy>
-                <div className={cx('actions')}></div>
+                <div className={cx('actions')}>
+                    <Button text>Upload</Button>
+                    <Button primary>
+                        {/* leftIcon={<FontAwesomeIcon icon={faSignIn} />} */}
+                        Log In
+                    </Button>
+                </div>
             </div>
         </header>
     );
